e12d26b0d81abde0f05202ca408e2ff2b4ba73b3
Gon Zifroni <gonzifroni@Gons-MacBook-Pro.local>
2013-09-23 20:33:14 -0700

    Added monitrc templates for Monit to run Redis, a Resque Scheduler and a Resque worker.

--- recipes/default.rb
+++ recipes/default.rb
@@ -59,3 +59,16 @@ node["monit"]["default_monitrc_configs"].each do |conf|
     notifies :restart, "service[monit]", :delayed
   end
 end
+
+monit_monitrc "redis" do
+  variables({ app_path: '/srv/www/codelist/current' })
+end
+
+monit_monitrc "resque-worker" do
+  variables({ worker_count: 1,
+              app_path: '/srv/www/codelist/current' })
+end
+
+monit_monitrc "resque-scheduler" do
+  variables({ app_path: '/srv/www/codelist/current' })
+end
